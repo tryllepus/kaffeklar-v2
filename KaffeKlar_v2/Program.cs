1c5e960aea89b581171780c6b09604f2656ccb15
tryllepus <daniel.ph24@gmail.com>
2024-10-14 21:42:50 +0200

    init

---
+++ KaffeKlar_v2/Program.cs
@@ -0,0 +1,39 @@
+using KaffeKlar_v2.Client.Pages;
+using KaffeKlar_v2.Components;
+using MudBlazor.Services;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRadzenComponents();
+builder.Services.AddMudServices();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(KaffeKlar_v2.Client._Imports).Assembly);
+
+app.Run();
